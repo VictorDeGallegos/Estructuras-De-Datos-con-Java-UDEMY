2f05c2ff5cac218ada6b7b096d2ccbc74670ea05
VictorDeGallegos <316160456@ciencias.unam.mx>
2021-12-06 11:56:31 -0600

    Actualizacion de repo

--- Ejercicios/src/colas/ColaMain.java
+++ Ejercicios/src/colas/ColaMain.java
@@ -16,33 +16,33 @@ public class ColaMain {
             System.out.print(n);
             System.out.print(" ");
         }
+        System.out.println(" ");
+        // // insertamos 5 elementos
+        // cola.insertar("Perro");
+        // cola.insertar("Gato");
+        // cola.insertar("Caballo");
+        // cola.insertar("Leon");
+        // cola.insertar("Tigre");
 
-        // insertamos 5 elementos
-        cola.insertar("Perro");
-        cola.insertar("Gato");
-        cola.insertar("Caballo");
-        cola.insertar("Leon");
-        cola.insertar("Tigre");
+        // System.out.println("");
+        // System.out.print("ELEMENTO EN LA CABEZA DE LA COLA: ");
+        // System.out.print(cola.frenteCola());
+        // System.out.println("");
 
-        System.out.println("");
-        System.out.print("ELEMENTO EN LA CABEZA DE LA COLA: ");
-        System.out.print(cola.frenteCola());
-        System.out.println("");
+        // System.out.print("ESTA COLA VACIA? ");
+        // System.out.println(cola.colaVacia());
 
-        System.out.print("ESTA COLA VACIA? ");
-        System.out.println(cola.colaVacia());
+        // System.out.print("ESTA LA COLA LLENA? ");
+        // System.out.println(cola.colaLLena());
 
-        System.out.print("ESTA LA COLA LLENA? ");
-        System.out.println(cola.colaLLena());
+        // System.out.print("TAMAÑO DE LA COLA: ");
+        // System.out.println(cola.tamanoCola());
 
-        System.out.print("TAMAÑO DE LA COLA: ");
-        System.out.println(cola.tamanoCola());
-
-        while (!cola.colaVacia()) {
-            Object m = cola.quitar();
-            System.out.print(m);
-            System.out.print(" ");
-        }
-        System.out.println("");
+        // while (!cola.colaVacia()) {
+        // Object m = cola.quitar();
+        // System.out.print(m);
+        // System.out.print(" ");
+        // }
+        // System.out.println("");
     }
 }
